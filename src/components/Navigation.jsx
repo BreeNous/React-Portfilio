f63f645fbc1fe38f59915e4d6eee1618d68ca5f8
BreeNous <breeanna.nouskhajian@gmail.com>
2024-07-05 17:48:06 -0500

    changed header nav layout to new design, still needs some tweaks and color and breakpoints

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,36 +1,56 @@
 
 import { Link, useLocation } from 'react-router-dom';
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
 function NavTabs() {
     const currentPage = useLocation().pathname;
 
     return (
 
-        <ul className="nav nav-tabs flex">
 
-            <li className="nav-item mr-5">
-                <Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>Home</Link>
-            </li>
+                <ul className="nav nav-tabs flex w-1/4 h-full justify-between">
 
-            <li className="nav-item mr-5">
-                <Link to="/About" className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}>About</Link>
-            </li>
+                    <li>
+                        <Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>
+                            <div className="bg-slate-300 w-10 h-full">
+                                <p className='rotate-90 pl-5'>Home</p>
+                            </div>
+                        </Link>
+                    </li>
 
-            <li className="nav-item mr-5">
-                <Link to="/Portfolio" className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</Link>
-            </li>
+                    <li>
+                        <Link to="/About" className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}>
+                            <div className="bg-slate-300 w-10 h-full">
+                                <p className='rotate-90 pl-5'>About</p>
+                            </div>
+                        </Link>
+                    </li>
 
-            <li className="nav-item mr-5">
-            <Link to="/Contact" className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}>Contact</Link>
-            </li>
+                    <li>
+                        <Link to="/Portfolio" className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}>
+                            <div className="bg-slate-300 w-10 h-full">
+                                <p className='rotate-90 pl-5'>Portfolio</p>
+                            </div>
+                        </Link>
+                    </li>
 
-            <li className="nav-item">
-            <Link to="/Resume" className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}>Resume</Link>
-            </li>
+                    <li>
+                        <Link to="/Contact" className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}>
+                            <div className="bg-slate-300 w-10 h-full">
+                                <p className='rotate-90 pl-5'>Contact</p>
+                            </div>
+                        </Link>
+                    </li>
 
-        </ul>
+                    <li>
+                        <Link to="/Resume" className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}>
+                            <div  className="bg-slate-300 w-10 h-full">
+                                <p className='rotate-90 pl-5'>Resume</p>
+                            </div>
+                        </Link>
+                    </li>
+
+                </ul>
+
 
     );
 }
