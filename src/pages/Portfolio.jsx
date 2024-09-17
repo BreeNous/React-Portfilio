0b1e3ca54e49c90ddae4996a571bf29ce33f6cc3
BreeNous <breeanna.nouskhajian@gmail.com>
2024-09-17 17:24:05 -0500

    working on revamping portfolio section, trying to get a hover animation working but have to stop in the middle of figuring it out

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,17 +1,44 @@
-import Card from "../components/Card.jsx"
-import ComingSoon from "../components/ComingSoon.jsx"
 
 function Portfolio() {
     
+    const cardHover = function() {
+        onmouseover
+    
+        return cardHover
+    }
+
     return(
         
-        <div className="flex flex-wrap h-full justify-center items-center">
-            <Card src="../assets/jeans-vid.mp4" type="video/mp4" link="https://frenzie24.github.io/jeans/" h2="Example 1" h3="Cool"/>
-            <Card src="../assets/weather-vid.mp4" type="video/mp4" link="https://breenous.github.io/Weather-Dashboard" h2="Example 2" h3="Sick"/>
-            <Card src="../assets/blog-vid.mp4" type="video/mp4" link="https://breenous.github.io/Personal-Blog/index.html" h2="Example 3" h3="Awesome"/>
-            <Card src="../assets/noteTaker-vid.mp4" type="video/mp4" link="https://breenous.github.io/Note-Taker/" h2="Example 4" h3="Rad"/>
-            <ComingSoon src="../assets/coming-soon.jpg" alt="coming soon" h2="Coming soon" h3="TBD"/>
-            <ComingSoon src="../assets/coming-soon.jpg" alt="coming soon" h2="Coming soon" h3="TBD"/>
+        // MAIN PAGE
+        <div className="flex h-full w-full">
+
+            {/* LEFT SIDE */}
+            {/* coloumn for left side */}
+            <div className="flex flex-col h-full w-1/2 items-center justify-evenly relative">
+            <div id="card">
+            {cardHover()}
+                {/* inside / back of envelope */}
+                    <div className="relative z-10 bg-stone-200 w-96 h-56 items-end">
+                        {/* front pieces of envelope */}
+                        <div>
+                            <div className="absolute z-30 left-0 bg-white-texture w-10 h-56 rounded-tr-2xl"></div>
+                            <div className="absolute z-30 bottom-0 bg-white-texture w-full h-48"></div>
+                            <div className="absolute z-30 right-0 bg-white-texture w-10 h-56 rounded-tl-2xl"></div>
+                        </div>
+                        {/* inner card */}
+                        <div className="flex justify-center">
+                            <div className="absolute z-20 bottom-4 h-full w-72 bg-green-texture rounded" id="innerCard"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-pink-50 w-96 h-56"></div>
+            </div>
+
+            {/* RIGHT SIDE */}
+            <div className="flex flex-col h-full w-1/2 items-center justify-evenly">
+                <div className="bg-pink-50 w-96 h-56"></div>
+                <div className="bg-pink-50 w-96 h-56"></div>
+            </div>
         </div>
         
     );
